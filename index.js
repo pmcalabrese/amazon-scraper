ffda803e24622b4fe40d4c754dce451f6fdc4639
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2016-10-10 22:17:28 +0200

    add config file and update package.json

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ var xray = Xray();
 var Table = require('cli-table');
 var Promise = require('promise');
 var request = require('request');
+var numeral = require('numeral');
+const config = require('./config.json');
 
-var lang = ['de', 'it', 'co.uk', 'fr']; // domain
-var product_url = '/Fujifilm-X-E2S-Appareil-Hybride-Objectif/dp/B01AP7S4PK/ref=sr_1_2?ie=UTF8&qid=1475777231&sr=8-2&keywords=fuji+x-e2s'
-
-const product = {
-    url: product_url,
-    name: 'Fujifilm XE2s'
-}
+const file_path = './price.txt';
 
 var price_table = [];
 var i = 0;
@@ -23,50 +19,70 @@ function exchange_rate(from_currency, to_currency, cb) {
     })
 }
 
+function euroUnformatter(text) {
+    var new_text = text.replace('.','').replace(' ','').replace(',','.')
+    return numeral().unformat(new_text)
+}
+
 function scrape(language) {
     return new Promise(function (resolve, reject) {
-        xray('https://www.amazon.' + language + product.url, '#priceblock_ourprice')(function(err, data) {
+        xray('https://www.amazon.' + language + config.product_url, {
+            price: '#priceblock_ourprice',
+            product_name: '#productTitle'
+        })(function(err, data) {
             if (err) reject(err);
             var data_to_return = {
+                datetime: new Date().toLocaleDateString('da'),
                 language: language,
-                name: product.name,
-                price: data
+                name: data.product_name.replace('\n', ' ').trim(),
+                price: data.price
             };
-            if (data.indexOf('£') !== -1) {
+            if (data.price.indexOf('£') !== -1) {
                 exchange_rate('EUR', 'GBP', function(er) {
-                    var i = data.indexOf('£');
-                    price = parseFloat(data.substring(i+1)) / parseFloat(er);
+                    var i = data.price.indexOf('£');
+                    price = parseFloat(data.price.substring(i+1)) / parseFloat(er);
                     data_to_return.price_eur = price
                     resolve(data_to_return);
                 })
             } else {
-                data_to_return.price_eur = data;
+                data_to_return.price_eur = euroUnformatter(data.price);
                 resolve(data_to_return);
             }
         })
     })
 }
 
+function sortByPrice(a, b) {
+  if (a.price_eur < b.price_eur) return -1;
+  if (a.price_eur > b.price_eur) return 1;
+  return 0;
+}
 
 // Build promises array
 var scrape_arr = []
-lang.forEach(function(language) {
+config.lang.forEach(function(language) {
     scrape_arr.push(scrape(language))
 })
 
 // When all request are done
 Promise.all(scrape_arr).then(function(data) {
+    printTable(data);
+}, function(err) {
+    console.log(err);
+})
+
+function printTable(data) {
+    data.sort(sortByPrice);
     var table = new Table({
-        head: ['Site', 'name', 'original price', 'price (EUR)'],
-        colWidths: [10, 20, 20 ,20]
+        head: ['Date', 'Site', 'Product name', 'Original price', 'Price (EUR)'],
+        colWidths: [20, 10, 20, 20 ,20]
     });
     data.forEach(function(product) {
         var arr = [];
         for (value in product) {
-            console.log(value);            
             arr.push(product[value]);
         }
         table.push(arr);
     })
     console.log(table.toString())
-})
+}
