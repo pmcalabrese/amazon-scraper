e6073434909a84988fddcc8f8e6c8a0347f149f2
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2016-10-13 09:28:17 +0200

    update test script

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var AmazonScraper = require('./index.js')
+const AmazonScraper = require('./index.js')
+const config = require('./config.json')
 
-var amazon_scraper = AmazonScraper('./config.json');
+const amazon_scraper = AmazonScraper(config);
 
 amazon_scraper.scraper.then(function(data) {
     amazon_scraper.printTable(data)
